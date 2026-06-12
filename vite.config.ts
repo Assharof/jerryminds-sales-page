e50fe819483814bcb8a5b7a88e9e72902dcfe49a
Muraiana Olamide Sherif <olly2ilevel@gmail.com>
2026-06-12 21:23:05 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { fileURLToPath } from "url";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
-  base: Process.env.VITE_BASE_PATH || "/jerryminds-sales-page"
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteSingleFile(),
+  ],
+  base: "/jerryminds-sales-page/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
